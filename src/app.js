78eb5202ae8a93be75301d18823d6a86bbef39f3
ralcadev09 <121913575+ralcadev09@users.noreply.github.com>
2025-04-29 04:07:29 +0000

    Proyecto sin puntos extra

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const numbers = ["A", "2", "3", "4", "5","6","7","8","9","10","J", "Q", "K"]
+const randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+
+const suits = ["♦", "♥", "♠", "♣"]
+const randomSuit = suits[Math.floor(Math.random() * suits.length)]
+
+window.onload = function () {
+    let number = document.querySelector("#number")
+    number.innerHTML = randomNumber
+if(randomSuit==="♥"||randomSuit==="♦"){
+  number.style.color = "red"
+}
+    let suitsElements = document.querySelectorAll(".suit")
+    for (let i = 0; i < suitsElements.length; i++) {
+      suitsElements[i].innerHTML = randomSuit
+      
+      if(randomSuit==="♥"||randomSuit==="♦"){
+        suitsElements[i].style.color = "red"
+      }
+    }
+
+
+
+  };
