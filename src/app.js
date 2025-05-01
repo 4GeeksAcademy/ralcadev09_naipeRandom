cc56f3317eaa76070dcb4849b92e3687eb4dd71e
ralcadev09 <121913575+ralcadev09@users.noreply.github.com>
2025-05-01 03:03:11 +0000

    Proyecto terminado con pts extra !

--- src/app.js
+++ src/app.js
@@ -5,27 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const numbers = ["A", "2", "3", "4", "5","6","7","8","9","10","J", "Q", "K"]
-const randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
 
 const suits = ["♦", "♥", "♠", "♣"]
-const randomSuit = suits[Math.floor(Math.random() * suits.length)]
 
 window.onload = function () {
-    let number = document.querySelector("#number")
-    number.innerHTML = randomNumber
-if(randomSuit==="♥"||randomSuit==="♦"){
-  number.style.color = "red"
+  generateCard();
+  setInterval(generateCard, 10000);
 }
-    let suitsElements = document.querySelectorAll(".suit")
-    for (let i = 0; i < suitsElements.length; i++) {
-      suitsElements[i].innerHTML = randomSuit
-      
-      if(randomSuit==="♥"||randomSuit==="♦"){
-        suitsElements[i].style.color = "red"
-      }
+
+window.generateCard = function () {
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)]
+
+
+  let number = document.querySelector("#number")
+  number.innerHTML = randomNumber
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    number.style.color = "red"
+  }
+  let suitsElements = document.querySelectorAll(".suit")
+  for (let i = 0; i < suitsElements.length; i++) {
+    suitsElements[i].innerHTML = randomSuit
+
+    if (randomSuit === "♥" || randomSuit === "♦") {
+      suitsElements[i].style.color = "red"
     }
+  }
+};
+window.applySize = function () {
+  const width = document.getElementById("widthInput").value;
+  const height = document.getElementById("heightInput").value;
+  const card = document.getElementById("card");
 
-
-
-  };
+  if (width) card.style.width = width;
+  if (height) card.style.height = height;
+};
